064a77872db87f671656efbb05a88518455b1992
Stas Dzasokhov <stsdz@rambler.ru>
2024-01-30 20:18:42 +0300

    Add scheduler

--- bot/theatre.py
+++ bot/theatre.py
@@ -1,9 +1,11 @@
 import asyncio
 import string
+from datetime import timedelta
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import config
 from parser.service import TheatreParser
@@ -13,10 +15,13 @@ from templates import get_event_template
 bot = Bot(config.BOT_TOKEN, parse_mode='html')
 dp = Dispatcher()
 parser = TheatreParser()
+scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 
 
 @dp.message(CommandStart())
 async def start(message: Message):
+    scheduler.add_job(send_message_interval, 'interval', seconds=int(config.UPDATE_TIME_INTERVAL),
+                      kwargs={'message': message})
     await message.answer("Aloha!", reply_markup=main_kb)
 
 
@@ -34,14 +39,19 @@ async def events(message: Message):
 async def events(message: Message):
     string.Template(message.text).safe_substitute()
     theatre_events = parser.get_events_json()['events']
-    response_text = 'Афиша:\n'
+    response_text = f'{message.chat.id}\nАфиша:\n'
     for event in theatre_events:
         response_text += get_event_template(event)
         response_text += '-' * 24
     await message.answer(response_text)
 
 
+async def send_message_interval(message: Message):
+    await message.answer("ALarm!")
+
+
 async def main():
+    scheduler.start()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
